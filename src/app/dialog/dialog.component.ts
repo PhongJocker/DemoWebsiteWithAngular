e1b4341dfa366c25659230863b373ad110e76a31
PhongJocker <phongjocker.1304.2k1@gmail.com>
2022-03-20 14:05:22 +0700

    Demo-Script-Add: Thêm xử lý sự kiện Thêm / Xóa / Sửa

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,3 +1,5 @@
+import { DataService } from './../data.service';
+import { TreeViewComponent } from '../tree-view/tree-view.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
-  constructor() { }
-
+  public treeView = new TreeViewComponent(this.dataService);
+  constructor(public dataService: DataService) { }
+  
   ngOnInit(): void {
+  }
+
+  save(id: string, name: string, description: string, note: string, serial: string) {
+    this.dataService.showAddForm = false;
+    this.treeView.rightdata = this.dataService.temp;
+    
+    if (this.dataService.edit) {
+      this.treeView.edit({id: id, name: name, description: description, note: note, serial: serial, child: []})
+      this.dataService.edit = false;
+    } else {
+      this.treeView.add({id: id, name: name, description: description, note: note, serial: serial, child: []});
+    }
+  }
+  
+  remove(){
+    this.dataService.showRemoveConfirm = false;
+    this.treeView.rightdata = this.dataService.temp;
+    this.treeView.remove(this.dataService.index);
   }
 
 }
